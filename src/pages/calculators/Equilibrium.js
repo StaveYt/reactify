2cf8cdc61dcf31e5794f9599339fd45623e9ba29
StaveYt <stavex112yt@gmail.com>
2023-11-13 10:55:57 +0100

    chemistry equation - reactants

--- src/pages/calculators/Equilibrium.js
+++ src/pages/calculators/Equilibrium.js
@@ -1,4 +1,34 @@
+import { useState } from "react"
+import ReactionParticipants from "../../components/equation/ReactionParticipants"
+
 function EquilibriumCalc(){
+    const [reactants, setReactants] = useState([0])
+    const [products, setProducts] = useState([0])
+    function handleAddReactant(event){
+        console.log(reactants)
+        event.target.className+=" hidden"
+        let reactants2 = reactants
+        reactants2.push(1)
+        console.log(reactants2)
+        setReactants(reactants2)
+
+    }
+    function handleDeleteReactant(event){
+        console.log(event)
+        event.target.className+=" hidden"
+
+    }
+    function handleAddProduct(event){
+        console.log(event)
+        event.target.className+=" hidden"
+
+    }
+    function handleDeleteProduct(event){
+        console.log(event)
+        event.target.className+=" hidden"
+
+    }
+
     return(
         <div className="flex flex-col mx-4">
             <div>
@@ -6,11 +36,15 @@ function EquilibriumCalc(){
             </div>
             <h2>Upiši kemijsku jednadžbu</h2>
             <div className="h-20 grid grid-cols-7">
-                <div className="border border-rose-600" style={{gridColumnStart:"1",gridColumnEnd:"4"}}></div>
+                <div className="border overflow-auto border-rose-600 flex flex-row" style={{gridColumnStart:"1",gridColumnEnd:"4"}}>
+                    {reactants.map((el,ind)=>(
+                        <ReactionParticipants key={ind} addRAndP={handleAddReactant} deleteRAndP={handleDeleteReactant}/>
+                    ))}
+                </div>
                 <div className="border-green-600 border"></div>
                 <div className="border-rose-600 border" style={{gridColumnStart:"5",gridColumnEnd:"8"}}></div>
             </div>
-            <button>Izračunaj</button>
+            <button className="p-1 rounded-sm bg-green-600 text-white border border-green-500">Izračunaj</button>
             <div></div>
         </div>
     )
